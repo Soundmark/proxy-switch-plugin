c458a9d7adc31148030ec9b599a49b8cd6ece014
郑家豪 <beatboxzjh@163.com>
2022-03-30 16:48:55 +0800

    refactor: refactor with jsDoc

--- src/ProxySelect.js
+++ src/ProxySelect.js
@@ -1,26 +1,23 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var select = document.createElement("select");
+const select = document.createElement("select");
 fetch("/proxy/list")
-    .then(function (res) { return res.json(); })
-    .then(function (data) {
-    var _a;
-    if ((_a = data === null || data === void 0 ? void 0 : data.list) === null || _a === void 0 ? void 0 : _a.length) {
-        var innerHtml_1 = "";
-        data.list.forEach(function (item) {
-            innerHtml_1 += "<option value=".concat(item, ">").concat(item, "</option>");
-        });
-        select.innerHTML = innerHtml_1;
-        var sessionProxyKey = sessionStorage.getItem("proxy-key");
-        if ((data === null || data === void 0 ? void 0 : data.defaultProxy) && !sessionProxyKey) {
-            sessionStorage.setItem("proxy-key", data.defaultProxy);
-        }
-        select.value = sessionProxyKey || (data === null || data === void 0 ? void 0 : data.defaultProxy) || data.list[0];
+  .then((res) => res.json())
+  .then((data) => {
+    if (data?.list?.length) {
+      let innerHtml = "";
+      data.list.forEach((item) => {
+        innerHtml += `<option value=${item}>${item}</option>`;
+      });
+      select.innerHTML = innerHtml;
+      const sessionProxyKey = sessionStorage.getItem("proxy-key");
+      if (data?.defaultProxy && !sessionProxyKey) {
+        sessionStorage.setItem("proxy-key", data.defaultProxy);
+      }
+      select.value = sessionProxyKey || data?.defaultProxy || data.list[0];
     }
-});
-select.onchange = function (e) {
-    sessionStorage.setItem("proxy-key", e.target.value);
-    fetch("/proxy/change?proxy=".concat(e.target.value));
+  });
+select.onchange = (e: any) => {
+  sessionStorage.setItem("proxy-key", e.target.value);
+  fetch(`/proxy/change?proxy=${e.target.value}`);
 };
-exports.default = select;
-//# sourceMappingURL=ProxySelect.js.map
+
+export default select;
--- src/ProxySwitchPlugin.js
+++ src/ProxySwitchPlugin.js
@@ -1,112 +1,84 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
-var Server;
-var chalk = require("chalk");
+let Server;
+const chalk = require("chalk");
 try {
-    Server = require("webpack-dev-server");
+  Server = require("webpack-dev-server");
+} catch (e) {
+  console.log(
+    chalk.yellow(
+      "Warning: Cannot find module webpack-dev-server, try deliver it via parameter"
+    )
+  );
 }
-catch (e) {
-    console.log(chalk.yellow("Warning: Cannot find module webpack-dev-server, try deliver it via parameter"));
-}
-var ProxySwitchPlugin = (function () {
-    function ProxySwitchPlugin(option, DevServer) {
-        this.option = option;
-        if (!Server) {
-            if (DevServer) {
-                Server = DevServer;
-            }
-            else {
-                throw new Error(chalk.red('Error: Cannot find Server from parameter, try "npm i webpack-dev-server -D" to solve this problem'));
-            }
-        }
+
+/**
+ * @typedef {Object} Option
+ * @property {Array} [proxyList]
+ * @property {string} [defaultProxy]
+ */
+class ProxySwitchPlugin {
+  /** @type {Option} */
+  option;
+  /** @type {number} */
+  baseRouteStackLength;
+
+  /**
+   * @param {Option} option
+   * @param {*} DevServer
+   */
+  constructor(option, DevServer) {
+    this.option = option;
+    if (!Server) {
+      if (DevServer) {
+        Server = DevServer;
+      } else {
+        throw new Error(
+          chalk.red(
+            'Error: Cannot find Server from parameter, try "npm i webpack-dev-server -D" to solve this problem'
+          )
+        );
+      }
     }
-    ProxySwitchPlugin.prototype.apply = function () {
-        var setupMiddlewares = Server.prototype.setupMiddlewares;
-        var normalizeOptions = Server.prototype.normalizeOptions;
-        var option = this.option;
-        var proxyKeys = Object.keys((option === null || option === void 0 ? void 0 : option.proxyList) || {});
-        Server.prototype.normalizeOptions = function () {
-            return __awaiter(this, void 0, void 0, function () {
-                return __generator(this, function (_a) {
-                    switch (_a.label) {
-                        case 0:
-                            if (proxyKeys.length) {
-                                this.options.proxy =
-                                    option.proxyList[(option === null || option === void 0 ? void 0 : option.defaultProxy) || proxyKeys[0]];
-                            }
-                            return [4, normalizeOptions.call(this)];
-                        case 1:
-                            _a.sent();
-                            return [2];
-                    }
-                });
-            });
-        };
-        Server.prototype.setupMiddlewares = function (middlewares, devServer) {
-            var _this = this;
-            this.app.get("/proxy/list", function (req, res) {
-                res.status(200).json({
-                    list: proxyKeys,
-                    defaultProxy: option.defaultProxy,
-                });
-            });
-            this.app.get("/proxy/change", function (req, res) { return __awaiter(_this, void 0, void 0, function () {
-                var proxy;
-                var _a;
-                return __generator(this, function (_b) {
-                    switch (_b.label) {
-                        case 0:
-                            proxy = req.query.proxy;
-                            this.options.proxy = (_a = option === null || option === void 0 ? void 0 : option.proxyList) === null || _a === void 0 ? void 0 : _a[proxy];
-                            return [4, normalizeOptions.call(this)];
-                        case 1:
-                            _b.sent();
-                            this.app._router.stack = this.app._router.stack.slice(0, this.baseRouteStackLength);
-                            setupMiddlewares.call(this, middlewares, devServer);
-                            res.status(200);
-                            return [2];
-                    }
-                });
-            }); });
-            this.baseRouteStackLength = this.app._router.stack.length;
-            setupMiddlewares.call(this, middlewares, devServer);
-        };
+  }
+
+  apply() {
+    const setupMiddlewares = Server.prototype.setupMiddlewares;
+    const normalizeOptions = Server.prototype.normalizeOptions;
+    const option = this.option;
+    const proxyKeys = Object.keys(option?.proxyList || {});
+
+    Server.prototype.normalizeOptions = async function () {
+      if (proxyKeys.length) {
+        this.options.proxy =
+          option.proxyList[option?.defaultProxy || proxyKeys[0]];
+      }
+      await normalizeOptions.call(this);
     };
-    return ProxySwitchPlugin;
-}());
+
+    Server.prototype.setupMiddlewares = function (middlewares, devServer) {
+      this.app.get("/proxy/list", (req, res) => {
+        res.status(200).json({
+          list: proxyKeys,
+          defaultProxy: option.defaultProxy,
+        });
+      });
+      this.app.get("/proxy/change", async (req, res) => {
+        const { proxy } = req.query;
+        console.log(proxy);
+        this.options.proxy = option?.proxyList?.[proxy];
+        await normalizeOptions.call(this);
+        this.app._router.stack = this.app._router.stack.slice(
+          0,
+          this.baseRouteStackLength
+        );
+        setupMiddlewares.call(this, middlewares, devServer);
+        res.status(200);
+      });
+
+      this.baseRouteStackLength = this.app._router.stack.length;
+
+      setupMiddlewares.call(this, middlewares, devServer);
+    };
+  }
+}
+
 module.exports = ProxySwitchPlugin;
-//# sourceMappingURL=ProxySwitchPlugin.js.map
