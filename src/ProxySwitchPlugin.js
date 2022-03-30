47a4ef7f24073130a93e6082920539ee05b62330
郑家豪 <beatboxzjh@163.com>
2022-03-30 22:21:02 +0800

    feat: support webpack-dev-server version3

--- src/ProxySwitchPlugin.js
+++ src/ProxySwitchPlugin.js
@@ -41,42 +41,74 @@ class ProxySwitchPlugin {
   }
 
   apply() {
-    const setupMiddlewares = Server.prototype.setupMiddlewares;
-    const normalizeOptions = Server.prototype.normalizeOptions;
     const option = this.option;
     const proxyKeys = Object.keys(option?.proxyList || {});
 
-    Server.prototype.normalizeOptions = async function () {
-      if (proxyKeys.length) {
-        this.options.proxy =
-          option.proxyList[option?.defaultProxy || proxyKeys[0]];
-      }
-      await normalizeOptions.call(this);
-    };
+    // version 3
+    if (typeof Server.prototype.setupFeatures === "function") {
+      const setupFeatures = Server.prototype.setupFeatures;
+      this.options.proxy =
+        option.proxyList[option?.defaultProxy || proxyKeys[0]];
 
-    Server.prototype.setupMiddlewares = function (middlewares, devServer) {
-      this.app.get("/proxy/list", (req, res) => {
-        res.status(200).json({
-          list: proxyKeys,
-          defaultProxy: option.defaultProxy,
+      Server.prototype.setupFeatures = function () {
+        this.app.get("/proxy/list", (req, res) => {
+          res.status(200).json({
+            list: proxyKeys,
+            defaultProxy: option.defaultProxy,
+          });
         });
-      });
-      this.app.get("/proxy/change", async (req, res) => {
-        const { proxy } = req.query;
-        this.options.proxy = option?.proxyList?.[proxy];
+        this.app.get("/proxy/change", async (req, res) => {
+          const { proxy } = req.query;
+          this.options.proxy = option?.proxyList?.[proxy];
+          this.app._router.stack = this.app._router.stack.slice(
+            0,
+            this.baseRouteStackLength
+          );
+          setupFeatures.call(this);
+          res.status(200);
+        });
+
+        this.baseRouteStackLength = this.app._router.stack.length;
+
+        setupFeatures.call(this);
+      };
+    } else {
+      //version 4
+      const setupMiddlewares = Server.prototype.setupMiddlewares;
+      const normalizeOptions = Server.prototype.normalizeOptions;
+
+      Server.prototype.normalizeOptions = async function () {
+        if (proxyKeys.length) {
+          this.options.proxy =
+            option.proxyList[option?.defaultProxy || proxyKeys[0]];
+        }
         await normalizeOptions.call(this);
-        this.app._router.stack = this.app._router.stack.slice(
-          0,
-          this.baseRouteStackLength
-        );
+      };
+
+      Server.prototype.setupMiddlewares = function (middlewares, devServer) {
+        this.app.get("/proxy/list", (req, res) => {
+          res.status(200).json({
+            list: proxyKeys,
+            defaultProxy: option.defaultProxy,
+          });
+        });
+        this.app.get("/proxy/change", async (req, res) => {
+          const { proxy } = req.query;
+          this.options.proxy = option?.proxyList?.[proxy];
+          await normalizeOptions.call(this);
+          this.app._router.stack = this.app._router.stack.slice(
+            0,
+            this.baseRouteStackLength
+          );
+          setupMiddlewares.call(this, middlewares, devServer);
+          res.status(200);
+        });
+
+        this.baseRouteStackLength = this.app._router.stack.length;
+
         setupMiddlewares.call(this, middlewares, devServer);
-        res.status(200);
-      });
-
-      this.baseRouteStackLength = this.app._router.stack.length;
-
-      setupMiddlewares.call(this, middlewares, devServer);
-    };
+      };
+    }
   }
 }
 
