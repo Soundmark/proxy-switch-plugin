f3ea9d3c3a80057981ec2f6e8e34625434c52fc6
郑家豪 <beatboxzjh@163.com>
2022-03-31 22:11:20 +0800

    refactor: remove es6 grammar

--- src/ProxySelect.js
+++ src/ProxySelect.js
@@ -3,17 +3,17 @@ select.classList.add("proxy-select");
 fetch("/proxy/list")
   .then((res) => res.json())
   .then((data) => {
-    if (data?.list?.length) {
+    if (data && data.list) {
       let innerHtml = "";
       data.list.forEach((item) => {
         innerHtml += `<option value=${item}>${item}</option>`;
       });
       select.innerHTML = innerHtml;
       const sessionProxyKey = sessionStorage.getItem("proxy-key");
-      if (data?.defaultProxy && !sessionProxyKey) {
+      if (data.defaultProxy && !sessionProxyKey) {
         sessionStorage.setItem("proxy-key", data.defaultProxy);
       }
-      select.value = sessionProxyKey || data?.defaultProxy || data.list[0];
+      select.value = sessionProxyKey || data.defaultProxy || data.list[0];
     }
   });
 select.onchange = (e) => {
