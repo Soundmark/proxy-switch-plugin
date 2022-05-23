ff33969a36d15a86634bfe1ad46c8a0f39a950bf
郑家豪 <beatboxzjh@163.com>
2022-05-23 18:20:03 +0800

    fix: utils proxyFactory fixed

--- src/utils.js
+++ src/utils.js
@@ -4,16 +4,6 @@ const proxyFactory = (proxyConfig = {}) => {
     return {
       changeOrigin: true,
       onProxyRes: (proxyRes, req) => {
-        const rewriteRules = Object.entries(options.pathRewrite || {});
-        let url = req.url;
-        for (let i = 0; i < rewriteRules.length; i++) {
-          const [matchPath, rewritePath] = rewriteRules[i];
-          const reg = new RegExp(matchPath);
-          if (url.match(reg).length) {
-            url = url.replace(reg, rewritePath);
-            break;
-          }
-        }
         proxyRes.headers["x-real-url"] =
           options.target.replace(/\/$/, "") + req.url;
       },
