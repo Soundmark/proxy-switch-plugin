e9dfbf734ea102f970762964452a2bea3bd4f749
郑家豪 <beatboxzjh@163.com>
2022-07-30 19:18:34 +0800

    fix(proxySelect):judge environment before create request

--- src/ProxySelect.js
+++ src/ProxySelect.js
@@ -1,24 +1,27 @@
 const select = document.createElement("select");
 select.classList.add("proxy-select");
-fetch("/proxy/list")
-  .then((res) => res.json())
-  .then((data) => {
-    if (data && data.list) {
-      let innerHtml = "";
-      data.list.forEach((item) => {
-        innerHtml += `<option value=${item}>${item}</option>`;
-      });
-      select.innerHTML = innerHtml;
-      const sessionProxyKey = sessionStorage.getItem("proxy-key");
-      if (data.defaultProxy && !sessionProxyKey) {
-        sessionStorage.setItem("proxy-key", data.defaultProxy);
+
+if (process.env.NODE_ENV === "development") {
+  fetch("/proxy/list")
+    .then((res) => res.json())
+    .then((data) => {
+      if (data && data.list) {
+        let innerHtml = "";
+        data.list.forEach((item) => {
+          innerHtml += `<option value=${item}>${item}</option>`;
+        });
+        select.innerHTML = innerHtml;
+        const sessionProxyKey = sessionStorage.getItem("proxy-key");
+        if (data.defaultProxy && !sessionProxyKey) {
+          sessionStorage.setItem("proxy-key", data.defaultProxy);
+        }
+        select.value = sessionProxyKey || data.defaultProxy || data.list[0];
       }
-      select.value = sessionProxyKey || data.defaultProxy || data.list[0];
-    }
-  });
-select.onchange = (e) => {
-  sessionStorage.setItem("proxy-key", e.target.value);
-  fetch(`/proxy/change?proxy=${e.target.value}`);
-};
+    });
+  select.onchange = (e) => {
+    sessionStorage.setItem("proxy-key", e.target.value);
+    fetch(`/proxy/change?proxy=${e.target.value}`);
+  };
+}
 
 export default select;
